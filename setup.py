336c92c393e506324353ff2428dffe907d3ac181
samseen <dmsameeram@gmail.com>
2026-01-02 00:39:41 +0530

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="imp-cli",
-    version="1.0.0",
+    version="1.0.1",
     description="IMP - Image Optimizer CLI Tool",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
